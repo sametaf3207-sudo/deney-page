b7b3bc1ff723778a2b3ae0bd56f0cb75b9733857
sametaf3207-sudo <sametaf3207@gmail.com>
2025-10-02 19:19:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 async def root():
     return FileResponse("static/sancak_vitrin_demo.html")
+if __name__ == "__main__":
+    import os
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
